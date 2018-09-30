40e628f9277d18f8bb5df9806af792ce00aa12c9
freshfineapple <pbeissinger@gmail.com>
2018-09-30 00:18:42 -0700

    Initial commit

---
+++ AbstractStatic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbstractStatic
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
